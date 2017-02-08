f24c3989fe601aa262028361db995e02dffe1963
Yoeran <contact@yoeran.nl>
2017-02-08 18:36:26 +0100

    Add ESLint. Fix ESLint errors. Add ESLint to pre-commit hook

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
-var path              = require( 'path' );
-var webpack           = require( 'webpack' );
-var merge             = require( 'webpack-merge' );
-var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-var autoprefixer      = require( 'autoprefixer' );
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-var CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-var entryPath         = path.join( __dirname, 'src/bootstrap.jsx' );
-var outputPath        = path.join( __dirname, 'dist' );
+var path = require("path");
+var webpack = require("webpack");
+var merge = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var autoprefixer = require("autoprefixer");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var entryPath = path.join(__dirname, "src/bootstrap.jsx");
+var outputPath = path.join(__dirname, "dist");
 
-console.log( 'WEBPACK GO!');
+console.log("WEBPACK GO!");
 
 // determine build env
-var TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
-var outputFilename = TARGET_ENV === 'production' ? '[name]-[hash].js' : '[name].js'
+var TARGET_ENV = process.env.npm_lifecycle_event === "build" ? "production" : "development";
+var outputFilename = TARGET_ENV === "production" ? "[name]-[hash].js" : "[name].js";
 
 // common webpack config
 var commonConfig = {
 
   output: {
-    path:       outputPath,
-    filename: `/static/js/${outputFilename}`,
-    // publicPath: '/'
+    path: outputPath,
+    filename: `/static/js/${outputFilename}`
   },
 
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
 
   module: {
@@ -32,42 +31,41 @@ var commonConfig = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.(eot|ttf|woff|woff2|svg)$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       }
     ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/static/index.html',
-      inject:   'body',
-      filename: 'index.html'
+      template: "src/static/index.html",
+      inject: "body",
+      filename: "index.html"
     })
   ],
 
-  postcss: [ autoprefixer( { browsers: ['last 2 versions'] } ) ],
+  postcss: [ autoprefixer({ browsers: ["last 2 versions"] }) ]
+};
 
-}
+// additional webpack settings for local env (when invoked by "npm start")
+if (TARGET_ENV === "development") {
+  console.log("Serving locally...");
 
-// additional webpack settings for local env (when invoked by 'npm start')
-if ( TARGET_ENV === 'development' ) {
-  console.log( 'Serving locally...');
-
-  module.exports = merge( commonConfig, {
+  module.exports = merge(commonConfig, {
 
     entry: [
-      'webpack-dev-server/client?http://localhost:8080',
+      "webpack-dev-server/client?http://localhost:8080",
       entryPath
     ],
 
     devServer: {
       // serve index.html in place of 404 responses
       historyApiFallback: true,
-      contentBase: './src',
+      contentBase: "./src"
     },
 
     module: {
@@ -75,10 +73,10 @@ if ( TARGET_ENV === 'development' ) {
         {
           test: /\.(css|scss)$/,
           loaders: [
-            'style-loader',
-            'css-loader',
-            'postcss-loader',
-            'sass-loader'
+            "style-loader",
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
           ]
         }
       ]
@@ -87,22 +85,21 @@ if ( TARGET_ENV === 'development' ) {
   });
 }
 
-// additional webpack settings for prod env (when invoked via 'npm run build')
-if ( TARGET_ENV === 'production' ) {
-  console.log( 'Building for prod...');
+// additional webpack settings for prod env (when invoked via "npm run build")
+if (TARGET_ENV === "production") {
+  console.log("Building for production...");
 
-  module.exports = merge( commonConfig, {
-
+  module.exports = merge(commonConfig, {
     entry: entryPath,
 
     module: {
       loaders: [
         {
           test: /\.(css|scss)$/,
-          loader: ExtractTextPlugin.extract( 'style-loader', [
-            'css-loader',
-            'postcss-loader',
-            'sass-loader'
+          loader: ExtractTextPlugin.extract("style-loader", [
+            "css-loader",
+            "postcss-loader",
+            "sass-loader"
           ])
         }
       ]
@@ -111,24 +108,24 @@ if ( TARGET_ENV === 'production' ) {
     plugins: [
       new CopyWebpackPlugin([
         {
-          from: 'src/static/img/',
-          to:   'static/img/'
+          from: "src/static/img/",
+          to: "static/img/"
         },
         {
-          from: 'src/favicon.ico'
-        },
+          from: "src/favicon.ico"
+        }
       ]),
 
       new webpack.optimize.OccurenceOrderPlugin(),
 
       // extract CSS into a separate file
-      new ExtractTextPlugin( 'static/css/[name]-[hash].css', { allChunks: true } ),
+      new ExtractTextPlugin("static/css/[name]-[hash].css", { allChunks: true }),
 
       // minify & mangle JS/CSS
       new webpack.optimize.UglifyJsPlugin({
-          minimize:   true,
-          compressor: { warnings: false }
-          // mangle:  true
+        minimize: true,
+        compressor: { warnings: false }
+        // mangle:  true
       })
     ]
 
